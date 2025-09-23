28035cf8858fb67a5bd8a72d1167836d31fe3014
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-09-24 01:18:20 +0300

    app name links to home

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 export const Navbar = () => {
   return (
     <div className="bg-gradient-to-b from-gray-950 to-gray-900  text-gray-200 p-4 flex justify-between">
-      <h1 className="font-mono text-2xl">Splitty</h1>
+      <Link href="/">
+        <h1 className="font-mono text-2xl">Splitty</h1>
+      </Link>
       <div className="flex justify-center items-center gap-2">
         <SignedOut>
           <SignInButton mode="modal">
