6155e71ad0a8b5d421e4b190f3c76aefa8beddf7
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-09-23 19:30:30 +0300

    styled pfp and added app name

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,31 @@
-import { SignInButton, SignedIn, SignedOut, SignOutButton, UserButton } from "@clerk/nextjs"
-import Link from "next/link"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 export const Navbar = () => {
-    return (
-        <div className="bg-gray-950 text-gray-200 p-4 flex justify-between">
-            <h1 className="text-2xl">Im navbar</h1>
-            <div className="flex justify-center items-center gap-2">
-                <SignedOut>
-                    <SignInButton mode="modal">
-                        <button className="px-2 py-1 rounded-lg bg-indigo-400 text-white font-medium
-                       hover:bg-indigo-500 focus:outline-none focus:ring-2
-                       focus:ring-indigo-400 focus:ring-offset-2 focus:ring-offset-gray-950
-                       transition-colors duration-200">Sign In!</button>
-                    </SignInButton>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="bg-gray-950 text-gray-200 p-4 flex justify-between">
+      <h1 className="font-mono text-2xl">Splitter</h1>
+      <div className="flex justify-center items-center gap-2">
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button
+              className="px-2 py-1 rounded-lg bg-teal-900 text-white font-medium
+                       hover:bg-teal-950 focus:outline-none focus:ring-2
+                       focus:ring-emerald-400 focus:ring-offset-2 focus:ring-offset-gray-950
+                       transition-colors duration-200"
+            >
+              Sign In!
+            </button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "border-2 border-white rounded-full",
+              },
+            }}
+          />
+        </SignedIn>
+      </div>
+    </div>
+  );
+};
