f3821d647bb94a89b651491e2599149e1022d7b6
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-09-30 20:09:07 +0300

    adding participant server function

--- src/app/bills/actions.ts
+++ src/app/bills/actions.ts
@@ -3,6 +3,13 @@
 import { deleteBill as deleteBillFromDb } from "@/prisma-db";
 import { CreateBillArgs } from "../components/addButton";
 import { createBill as createBillFromDb } from "@/prisma-db";
+import { addParticipantToBill as addParticipantToBillFromDb } from "@/prisma-db";
+
+export type JoinBillArgs = {
+  billCode: string;
+  userId: string;
+  displayName: string;
+};
 
 export async function deleteBill(id: number) {
   await deleteBillFromDb(id);
@@ -10,4 +17,8 @@ export async function deleteBill(id: number) {
 
 export async function createBill({ title, createdBy, displayName }: CreateBillArgs) {
   await createBillFromDb({ title, createdBy, displayName });
+}
+
+export async function addParticipantToBill({ billCode, userId, displayName}: JoinBillArgs) {
+  await addParticipantToBillFromDb({ billCode, userId, displayName})
 }
