d103166b61a1002b01a1f04fadc345271f778c77
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-10-02 19:51:15 +0300

    protecting bill routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const protectedRoutes = createRouteMatcher(["/user-profile", "/bills"])
+const protectedRoutes = createRouteMatcher(["/user-profile", "/bills(.*)"])
 
 export default clerkMiddleware(async (auth, req) => {
     if(protectedRoutes(req)) await auth.protect();
